b6655f3edee740a1a1942a0ffb98375afdf7d14e
Balkaneros91 <>
2023-06-10 13:06:37 +0000

    Try to add delete product file

--- products/urls.py
+++ products/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('add/', views.add_product, name='add_product'),
     path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
     path('delete/<int:product_id>/', views.delete_product, name='delete_product'),
+    # Add the path for the confirmation dialogue
+    path('delete/<int:product_id>/confirm/',
+         views.confirm_delete_product, name='confirm_delete_product'),
 ]
--- products/views.py
+++ products/views.py
@@ -132,6 +132,19 @@ def edit_product(request, product_id):
     return render(request, template, context)
 
 
+# @login_required
+# def delete_product(request, product_id):
+#     """ Delete a product from the store """
+#     if not request.user.is_superuser:
+#         messages.error(request, 'Sorry, only store owners can do that.')
+#         return redirect(reverse('home'))
+
+#     product = get_object_or_404(Product, pk=product_id)
+#     product.delete()
+#     messages.success(request, 'Product deleted!')
+#     return redirect(reverse('products'))
+
+
 @login_required
 def delete_product(request, product_id):
     """ Delete a product from the store """
@@ -139,7 +152,11 @@ def delete_product(request, product_id):
         messages.error(request, 'Sorry, only store owners can do that.')
         return redirect(reverse('home'))
 
-    product = get_object_or_404(Product, pk=product_id)
-    product.delete()
-    messages.success(request, 'Product deleted!')
-    return redirect(reverse('products'))
+    if request.method == 'POST':
+        product = get_object_or_404(Product, pk=product_id)
+        product.delete()
+        messages.success(request, 'Product deleted!')
+        return redirect(reverse('products'))
+    else:
+        # Handle GET request or any other condition
+        return redirect(reverse('products'))
